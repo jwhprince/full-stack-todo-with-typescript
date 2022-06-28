376f6476d29fabc2860fdfcf4f62d78928a4f0b5
jwhprince <piratkaignat@gmail.com>
2022-06-28 21:01:31 +0500

    clean input and readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# ToDo
+## Nest.js + PostgreSQL + React.js + TypeScript and some animation
+
+![](client/todo/public/task.gif)
+
+
+### Please check how to launch the application below:
+
+Client
+
+    npm install;
+    Launch frontend server with npm start;
+
+Server
+
+    npm install;
+    Create file .env according to envsimple.txt
+    Create DB;
+    Launch backend server with npm start;
+
--- client/todo/src/components/TodoForm/TodoForm.tsx
+++ client/todo/src/components/TodoForm/TodoForm.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useState } from "react"
+import React, { ChangeEvent, FormEvent, useRef, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { createTodo, showAlert } from "../../redux/actions"
 import { IAlertReducer } from "../../types/types"
@@ -6,42 +6,47 @@ import { Alert } from "../Alert/Alert"
 
 
 export const TodoForm = () => {
-    const [title, setTitle] = useState('')
-    const alertState = useSelector((state: IAlertReducer) => state.alertReducer)
-    const dispatch = useDispatch()
-    
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-       event.preventDefault()
+   const [title, setTitle] = useState('')
+   const ref = useRef<any>()
 
-       if (!title.trim()) {
+   const alertState = useSelector((state: IAlertReducer) => state.alertReducer)
+   const dispatch = useDispatch()
+
+   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault()
+
+
+      if (!title.trim()) {
          dispatch(showAlert('Task name cannot be empty', 'warning'))
          return
-       }
-       
-      
-       
-       dispatch(createTodo(title))
-       setTitle('')
-    }
+      }
 
-    const handleChangeInputValue = (event: ChangeEvent<HTMLInputElement>) => {
-        setTitle(event.target.value)
-    }
-    
-    return (
-    <form onSubmit={handleSubmit} >
-      {alertState.alertText.length > 0 && <Alert props={alertState} />}
+      dispatch(createTodo(title))
+      setTitle('')
+      ref.current.value = ''
+
+   }
+
+   const handleChangeInputValue = (event: ChangeEvent<HTMLInputElement>) => {
+      setTitle(event.target.value)
+   }
+
+
+   return (
+      <form onSubmit={handleSubmit} >
+         {alertState.alertText.length > 0 && <Alert props={alertState} />}
          <div className="mb-3 d-flex align-items-end justify-content-between">
-         <div className="form-group" style={{width: '92%', marginRight: '10px'}}>
-           <label htmlFor="" className="form-label">Add new task</label>
-           <input
-               onChange={handleChangeInputValue} 
-               type="text"
-               className="form-control"
-            />
-         </div>
+            <div className="form-group" style={{ width: '92%', marginRight: '10px' }}>
+               <label htmlFor="" className="form-label">Add new task</label>
+               <input
+                  onChange={handleChangeInputValue}
+                  ref={ref}
+                  type="text"
+                  className="form-control"
+               />
+            </div>
             <button className="btn btn-dark">create</button>
          </div>
-    </form>
-    )
+      </form>
+   )
 }
